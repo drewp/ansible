1159f6a64dd26debf048a18f4bee10868aeb9a69
drewp <drewp@bigasterisk.com>
2018-12-08 00:29:37 -0800

    cjdns docker build cleanups
    Ignore-this: a2d46324fe6ccabd287de0afbcf8765c
    
    darcs-hash:f95e9c94afc5c8af599fd5abe3e4be7980f38ec9

---
+++ roles/cjdns/files/crypto_types.h
@@ -0,0 +1,11 @@
+#ifndef crypto_types_h
+#define crypto_types_h
+typedef signed char crypto_int8;
+typedef unsigned char crypto_uint8;
+typedef short crypto_int16;
+typedef unsigned short crypto_uint16;
+typedef signed int crypto_int32;
+typedef unsigned int crypto_uint32;
+typedef signed long long crypto_int64;
+typedef unsigned long long crypto_uint64;
+#endif
